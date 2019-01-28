4358494462a2704b9bf3610a677cf9ef613098b5
Todd Kennedy <todd@selfassembled.org>
2019-01-28 15:23:42 -0800

    feat: set up proper fuse permissions
    Permissions must be passed to fuse to allow non-root access to the mounted
    path

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if [ $USE_FUSE = "true" ]; then
   if [ -f $GOPATH/bin/gcsfuse ]; then 
+    ES_UID=$(id -u elasticsearch)
+    ES_GID=$(id -g elasticsearch)
     mkdir -p /mnt/backup
-    $GOPATH/bin/gcsfuse --key-file=$GCS_CREDENTIALS_PATH $GCS_BUCKET /mnt/backup
+    $GOPATH/bin/gcsfuse -o allow_other --uid $ES_UID --gid $ES_GID --key-file=$GCS_CREDENTIALS_PATH $GCS_BUCKET /mnt/backup
   fi
 fi
 /docker-entrypoint.sh elasticsearch $ES_FLAGS
